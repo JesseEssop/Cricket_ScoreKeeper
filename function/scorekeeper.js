17b871d56dddc3930b6cad76d13bbb28bd54134a
JesseEssop <jsteez16@gmail.com>
2019-09-20 15:27:57 +0200

    test pass

--- function/scorekeeper.js
+++ function/scorekeeper.js
@@ -1,6 +1,76 @@
-module.exports = function ScoreKeeper(){
+module.exports = function CricketScoreKeeper() {
 
+    var scores = []
+    var showscore;
+    var scoresheet = [];
+    var total = 0;
+    var wickets = 10;
+    var over = 0;
+
+
+    function setOvers(set) {
+        over = set
+        // console.log(over)
+       
+    }
+
+    function howManyOvers(){
+        return over
+
+    }
+
+    function howManyWicktes() {
+        return wickets
+
+    }
+
+    function addScore(score) {
+        scores = score.split('');
+        if (over > 0) {
+            if (wickets > 0) {
+                for (var x = 1; x < scores.length; x++) {
+                    var wick = score.includes("w");
+                    showscore = scores[x];
+
+                    scoresheet.push(showscore);
+                }
+                if (wick == true) {
+                    wickets--
+                }
+
+            }
+           
+        } 
+        over--
+    }
+
+
+    function showScoreSheet() {
+        return scoresheet;
+    }
+
+    function CalculateScore() {
+
+        let numOr0 = n => isNaN(n) ? 0 : n
+
+        total = scoresheet.reduce((a, b) =>
+            Number(numOr0(a)) + Number(numOr0(b)))
+
+        // console.log(over)
+
+        return total
+    }
 
 
     
+
+
+    return {
+        setOvers,
+        addScore,
+        showScoreSheet,
+        CalculateScore,
+        howManyWicktes,
+        howManyOvers
+    }
 }
--- test/scorekeeper.test.js
+++ test/scorekeeper.test.js
@@ -2,18 +2,85 @@ const assert = require("assert");
 const scoreKeep = require("../function/scorekeeper.js");
 
 describe('Cricket Score tests', function () {
-    it('This tests should be able to set the call cost', function () {
-        let scoreKeep = scoreKeeper();
-        scoreKeeper.setCallCost(3.50)
-        assert.equal(3.50, scoreKeep.getCallCost());
+    
+    it('This tests should be able to set the amount of overs', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(5);
+        scoreKeeper.addScore("123-w6")
+        scoreKeeper.addScore("463-ww")
+        scoreKeeper.addScore("463-ww")
+       
+        assert.equal(2, scoreKeeper.howManyOvers() )
+       
+    });
+    
+    
+    it('This tests calulates the total amount of points for 1 over', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(1)
+        scoreKeeper.addScore("123-w6");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.showScoreSheet();
+        
+
+        assert.equal(11,  scoreKeeper.CalculateScore())
+       
     });
 
+    it('This tests calulates the total amount of runs for 3 overs', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(3)
+        
+        scoreKeeper.addScore("123-w6");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.showScoreSheet();
+       
 
-    it('This tests should be able to set the call and sms cost', function () {
-        let scoreKeep = scoreKeeper();
-        scoreKeep.setCallCost(3.50)
-        scoreKeep.setSmsCost(0.85)
-        assert.equal(3.50, scoreKeep.getCallCost());
-        assert.equal(0.85, scoreKeep.getSmsCost());
+        assert.equal( 29, scoreKeeper.CalculateScore())
+       
+    });
+
+    it('This tests calulates the total amount of runs for 5 overs and ignores the rest', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(5)
+        
+        scoreKeeper.addScore("123-w6");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+
+        scoreKeeper.showScoreSheet();
+       
+        assert.equal(47, scoreKeeper.CalculateScore())
+    });
+
+    it('This tests returns the amount of overs leftover', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(4)
+       
+        scoreKeeper.addScore("123-w6");
+        scoreKeeper.addScore("463-ww");
+       
+        assert.equal(2,  scoreKeeper.howManyOvers())
+       
+    });
+
+    it('This tests calulates the total amount of runs for 3 overs', function () {
+        let scoreKeeper = scoreKeep();
+        scoreKeeper.setOvers(5)
+        
+        scoreKeeper.addScore("123-w6");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.addScore("463-ww");
+        scoreKeeper.showScoreSheet();
+
+        assert.equal(5, scoreKeeper.howManyWicktes())
+       
     });
 });
